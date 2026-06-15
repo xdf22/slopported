d2a55479d3c6396a0a180a634a3c870ffffb2a26
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2026-06-15 16:26:37 -0500

    Delete PK3 before building if it exists

--- build.sh
+++ build.sh
@@ -7,9 +7,14 @@
 NAME="22backported2"
 VERSION="v0"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PK3NAME=$NAME-$VERSION.pk3
 
 printf '=====================================================================\n' >> $SCRIPT_DIR/pk3_output.log
 printf 'PK3 Compile Script for Linux/MacOS\nCreated by Ashi\n\nCurrent Directory: '$SCRIPT_DIR'\n\nCompiling' $NAME'-'$VERSION'.pk3' >> $SCRIPT_DIR/pk3_output.log
+
+if [ -e $PK3NAME ]; then
+rm $PK3NAME
+fi
 
 7z a $NAME-$VERSION.pk3 ./pk3/* -x!.bak -x!.dbs -x!.DS_Store -x!.backup1 -x!.backup2 -x!.backup3 -tzip >> $SCRIPT_DIR/pk3_output.log
 printf '=====================================================================\n' >> $SCRIPT_DIR/pk3_output.log
