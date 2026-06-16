f8307274e0f249e591a3990727cbf7f5e4964028
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2026-06-16 16:25:34 -0500

    Properly use a custom gamedata

--- build.sh
+++ build.sh
@@ -16,5 +16,5 @@ if [ -e $PK3NAME ]; then
 rm $PK3NAME
 fi
 
-7z a $NAME-$VERSION.pk3 ./pk3/* -x!.bak -x!.dbs -x!.DS_Store -x!.backup1 -x!.backup2 -x!.backup3 -tzip >> $SCRIPT_DIR/pk3_output.log
+7z a $PK3NAME ./pk3/* -x!.bak -x!.dbs -x!.DS_Store -x!.backup1 -x!.backup2 -x!.backup3 -tzip >> $SCRIPT_DIR/pk3_output.log
 printf '=====================================================================\n' >> $SCRIPT_DIR/pk3_output.log
